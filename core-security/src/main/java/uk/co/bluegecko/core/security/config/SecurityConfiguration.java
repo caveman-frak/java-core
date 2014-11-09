63165c27fc02e132924da7af1fed0a015211a920
Caveman Frak <tim.pickard@bluegecko.co.uk>
2014-11-09 21:33:12 +0000

    add spring security

---
+++ core-security/src/main/java/uk/co/bluegecko/core/security/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2009, <a href="http://bluegecko.co.uk/core">Blue Gecko Limited</a>
+ */
+package uk.co.bluegecko.core.security.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+/**
+ * Core Configuration class
+ */
+@Configuration
+@EnableGlobalAuthentication
+@EnableGlobalMethodSecurity
+@EnableWebSecurity
+public class SecurityConfiguration
+{
+
+	/**
+	 * Default constructor
+	 */
+	public SecurityConfiguration()
+	{
+		super();
+	}
+
+}
