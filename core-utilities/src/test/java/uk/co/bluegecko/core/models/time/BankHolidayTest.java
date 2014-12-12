966ec71940a20f80969d8b006d045b02b0aad1cd
Caveman Frak <tim.pickard@bluegecko.co.uk>
2014-12-12 18:31:29 +0000

    add last of date tests

---
+++ core-utilities/src/test/java/uk/co/bluegecko/core/models/time/BankHolidayTest.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2009, <a href="http://bluegecko.co.uk/core">Blue Gecko Limited</a>
+ */
+package uk.co.bluegecko.core.models.time;
+
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.MonthDay;
+import java.time.temporal.ChronoField;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+@SuppressWarnings( "javadoc" )
+public class BankHolidayTest implements TemporalTestBase
+{
+
+	private BankHoliday christmasHoliday;
+	private DayOfMonth christmasDay;
+
+	@Before
+	public final void setUp()
+	{
+		christmasDay = new DayOfMonth( MonthDay.of( Month.DECEMBER, 25 ) );
+		christmasHoliday = new BankHoliday( WorkingDays.western(), christmasDay );
+	}
+
+	@Test
+	public final void testHolidayInWeek()
+	{
+		assertThat( christmasDay.adjustInto( YEAR_START ).get( ChronoField.DAY_OF_WEEK ),
+				is( DayOfWeek.TUESDAY.getValue() ) );
+		assertThat( christmasHoliday.adjustInto( YEAR_START ).get( ChronoField.DAY_OF_WEEK ),
+				is( DayOfWeek.TUESDAY.getValue() ) );
+	}
+
+	@Test
+	public final void testHolidayInWeekend()
+	{
+		final LocalDate yearStart2010 = YEAR_START.minusYears( 2 );
+		assertThat( christmasDay.adjustInto( yearStart2010 ).get( ChronoField.DAY_OF_WEEK ),
+				is( DayOfWeek.SATURDAY.getValue() ) );
+		assertThat( christmasHoliday.adjustInto( yearStart2010 ).get( ChronoField.DAY_OF_WEEK ),
+				is( DayOfWeek.MONDAY.getValue() ) );
+	}
+
+}
--- core-utilities/src/test/java/uk/co/bluegecko/core/models/time/WorkingDayAdjusterTest.java
+++ core-utilities/src/test/java/uk/co/bluegecko/core/models/time/WorkingDayAdjusterTest.java
@@ -4,8 +4,16 @@
 package uk.co.bluegecko.core.models.time;
 
 
-import static org.junit.Assert.fail;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.time.temporal.Temporal;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -13,10 +21,110 @@ import org.junit.Test;
 public class WorkingDayAdjusterTest implements TemporalTestBase
 {
 
-	@Test
-	public final void test()
+	private WorkingDays western;
+	private WorkingDayAdjuster following;
+	private WorkingDayAdjuster previous;
+	private WorkingDayAdjuster modifiedFollowing;
+	private WorkingDayAdjuster modifiedPrevious;
+
+	@Before
+	public final void setUp()
 	{
-		fail( "Not yet implemented" ); // TODO
+		western = WorkingDays.western();
+		following = WorkingDayAdjuster.following( western );
+		previous = WorkingDayAdjuster.previous( western );
+		modifiedFollowing = WorkingDayAdjuster.modifiedFollowing( western );
+		modifiedPrevious = WorkingDayAdjuster.modifiedPrevious( western );
+	}
+
+	@Test
+	public final void testFollowingUnadjusted()
+	{
+		final Temporal date = LocalDate.of( YEAR, Month.DECEMBER, 5 );
+		assertThat( date.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( date.get( ChronoField.MONTH_OF_YEAR ), is( Month.DECEMBER.getValue() ) );
+		assertThat( date.get( ChronoField.DAY_OF_MONTH ), is( 5 ) );
+		assertThat( date.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.WEDNESDAY.getValue() ) );
+		final Temporal result = following.adjustInto( date );
+		assertThat( result.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( result.get( ChronoField.MONTH_OF_YEAR ), is( Month.DECEMBER.getValue() ) );
+		assertThat( result.get( ChronoField.DAY_OF_MONTH ), is( 5 ) );
+		assertThat( result.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.WEDNESDAY.getValue() ) );
+	}
+
+	@Test
+	public final void testFollowing()
+	{
+		final Temporal date = LocalDate.of( YEAR, Month.DECEMBER, 1 );
+		assertThat( date.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( date.get( ChronoField.MONTH_OF_YEAR ), is( Month.DECEMBER.getValue() ) );
+		assertThat( date.get( ChronoField.DAY_OF_MONTH ), is( 1 ) );
+		assertThat( date.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.SATURDAY.getValue() ) );
+		final Temporal result = following.adjustInto( date );
+		assertThat( result.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( result.get( ChronoField.MONTH_OF_YEAR ), is( Month.DECEMBER.getValue() ) );
+		assertThat( result.get( ChronoField.DAY_OF_MONTH ), is( 3 ) );
+		assertThat( result.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.MONDAY.getValue() ) );
+	}
+
+	@Test
+	public final void testPreviousUnadjusted()
+	{
+		final Temporal date = LocalDate.of( YEAR, Month.DECEMBER, 5 );
+		assertThat( date.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( date.get( ChronoField.MONTH_OF_YEAR ), is( Month.DECEMBER.getValue() ) );
+		assertThat( date.get( ChronoField.DAY_OF_MONTH ), is( 5 ) );
+		assertThat( date.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.WEDNESDAY.getValue() ) );
+		final Temporal result = previous.adjustInto( date );
+		assertThat( result.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( result.get( ChronoField.MONTH_OF_YEAR ), is( Month.DECEMBER.getValue() ) );
+		assertThat( result.get( ChronoField.DAY_OF_MONTH ), is( 5 ) );
+		assertThat( result.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.WEDNESDAY.getValue() ) );
+	}
+
+	@Test
+	public final void testPrevious()
+	{
+		final Temporal date = LocalDate.of( YEAR, Month.DECEMBER, 1 );
+		assertThat( date.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( date.get( ChronoField.MONTH_OF_YEAR ), is( Month.DECEMBER.getValue() ) );
+		assertThat( date.get( ChronoField.DAY_OF_MONTH ), is( 1 ) );
+		assertThat( date.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.SATURDAY.getValue() ) );
+		final Temporal result = previous.adjustInto( date );
+		assertThat( result.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( result.get( ChronoField.MONTH_OF_YEAR ), is( Month.NOVEMBER.getValue() ) );
+		assertThat( result.get( ChronoField.DAY_OF_MONTH ), is( 30 ) );
+		assertThat( result.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.FRIDAY.getValue() ) );
+	}
+
+	@Test
+	public final void testModifiedPrevious()
+	{
+		final Temporal date = LocalDate.of( YEAR, Month.DECEMBER, 1 );
+		assertThat( date.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( date.get( ChronoField.MONTH_OF_YEAR ), is( Month.DECEMBER.getValue() ) );
+		assertThat( date.get( ChronoField.DAY_OF_MONTH ), is( 1 ) );
+		assertThat( date.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.SATURDAY.getValue() ) );
+		final Temporal result = modifiedPrevious.adjustInto( date );
+		assertThat( result.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( result.get( ChronoField.MONTH_OF_YEAR ), is( Month.DECEMBER.getValue() ) );
+		assertThat( result.get( ChronoField.DAY_OF_MONTH ), is( 3 ) );
+		assertThat( result.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.MONDAY.getValue() ) );
+	}
+
+	@Test
+	public final void testModifiedFollowing()
+	{
+		final Temporal date = LocalDate.of( YEAR, Month.SEPTEMBER, 30 );
+		assertThat( date.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( date.get( ChronoField.MONTH_OF_YEAR ), is( Month.SEPTEMBER.getValue() ) );
+		assertThat( date.get( ChronoField.DAY_OF_MONTH ), is( 30 ) );
+		assertThat( date.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.SUNDAY.getValue() ) );
+		final Temporal result = modifiedFollowing.adjustInto( date );
+		assertThat( result.get( ChronoField.YEAR ), is( YEAR ) );
+		assertThat( result.get( ChronoField.MONTH_OF_YEAR ), is( Month.SEPTEMBER.getValue() ) );
+		assertThat( result.get( ChronoField.DAY_OF_MONTH ), is( 28 ) );
+		assertThat( result.get( ChronoField.DAY_OF_WEEK ), is( DayOfWeek.FRIDAY.getValue() ) );
 	}
 
 }
