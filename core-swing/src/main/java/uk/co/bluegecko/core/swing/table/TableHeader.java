46d12bf85646eaadfc08afbc1ab30c16427fb379
Caveman Frak <tim.pickard@bluegecko.co.uk>
2016-03-02 15:12:20 +0000

    fix pom files update versions

--- core-swing/src/main/java/uk/co/bluegecko/core/swing/table/TableHeader.java
+++ core-swing/src/main/java/uk/co/bluegecko/core/swing/table/TableHeader.java
@@ -14,6 +14,7 @@ import javax.swing.table.TableColumnModel;
  * Implementation of a table header supporting column definitions and sorting headers.
  *
  * @param <T>
+ *            type of table data
  */
 public class TableHeader< T > extends JTableHeader
 {
@@ -24,7 +25,7 @@ public class TableHeader< T > extends JTableHeader
 
 	/**
 	 * Create a table header.
-	 * 
+	 *
 	 * @param columnModel
 	 *            the column model
 	 * @param rowSorter
